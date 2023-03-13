321450e7b9e6cdd815e51b1cdc5e6fc8606a89a0
DESKTOP-1A84R9B\ThaiPhong <kim_thai_phong@allgrow-labo.jp>
2023-03-13 17:37:59 +0700

    3nd commit

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,58 +1,121 @@
 $(document).ready(function () {
-    $(".c-mainvisual__slider").slick({
-      fade: true,
-      speed: 2000,
-      infinite: true,
-      autoplay: true,
-      arrows: false,
-    });
-
-    ScrollReveal({
-      reset: false,
-      distance: "25px",
-      duration: 1100,
-      origin: "bottom",
-    });
-
-    ScrollReveal().reveal(
-      ".c-about__content, .c-works__header, .c-works__introcontent, .c-work__photosheading,.c-work__subheading2, .c-work__content"
-    );
-});
-
-$(document).ready(function () {
-  const modalWork = $('.c-works__modal');
-  const btnWork = $('.c-works__img');
-  const spanWork = $('.c-works__modalclose');
-
-  btnWork.click(function () {
-    modalWork.show();
+  $(".c-mainvisual__slider").slick({
+    fade: true,
+    speed: 1100,
+    infinite: true,
+    autoplay: true,
+    arrows: false,
   });
 
-  spanWork.click(function () {
-    modalWork.hide();
+  ScrollReveal({
+    reset: false,
+    distance: "25px",
+    duration: 1100,
+    origin: "bottom",
+  });
+
+  ScrollReveal().reveal(
+    ".c-about, .c-works__header, .c-works__intro, .c-works__photo, .c-career__header, .c-career__banner, .c-career__plan, .c-career__service, .c-intaview__header, .c-intaview__cards, .c-intaview__represent, .c-recruit__header, .c-recruit__tabwrapper, .c-company__heading, .c-company__about, .c-footer, .c-footer__nav, .c-footer__copyright"
+  );
+})
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    const scrollDistance = $(window).scrollTop();
+
+    $(".page-section").each(function (i) {
+
+      if ($(this).position().top <= scrollDistance + 250 || scrollDistance == 0) {
+        $(".c-header__gnavlink.is-active").removeClass("is-active");
+        $(".c-header__gnavlink").eq(i).addClass("is-active")
+      }
+    })
+  }).scroll();
+})
+
+$(".c-about__scroll").click(function () {
+  $("html, body").animate({
+    scrollTop: $(".c-about").offset().top
+  }, 100)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/*-------------------------------------------------------------------------*/
+$(document).ready(function () {
+  $(".c-works__img").click(function (e) {
+    $(".c-works__modal").show();
+
+    $(".c-works__img").each((index, item) => {
+      if ($(e.target).is(item)) {
+        $(".c-works__modalcontainer").eq(index).show();
+        $("body").css("overflow", "hidden");
+      } else {
+        $(".c-works__modalcontainer").eq(index).hide();
+      }
+    });
+  });
+
+  $(".c-works__modalclose").click(function () {
+    $(".c-works__modal").hide();
+    $("body").css("overflow", "unset");
   });
 
   $(window).on('click', function (e) {
     if ($(e.target).is('.c-works__modal')) {
-      modalWork.hide();
+      $(".c-works__modal").hide();
+      $("body").css("overflow", "unset");
     }
   });
+});
 
-  const modalIntaview = $('.c-intaview__modal');
-  const btnIntaview = $('.c-intaview__cardimage');
-  const spanIntaview = $('.c-intaview__modalclose');
+/*-------------------------------------------------------------------------*/
+$(document).ready(function () {
+  $('.c-intaview__cardimage').click(function (e) {
+    $('.c-intaview__modal').show();
 
-  btnIntaview.click(function () {
-    modalIntaview.show();
+    $(".c-intaview__cardimage").each((index, item) => {
+      if ($(e.target).is(item)) {
+        $(".c-intaview__modalmain").eq(index).show();
+        $("body").css("overflow", "hidden");
+      } else {
+        $(".c-intaview__modalmain").eq(index).hide();
+      }
+    });
   });
 
-  spanIntaview.click(function () {
-    modalIntaview.hide();
+
+  $('.c-intaview__modalclose').click(function () {
+    $('.c-intaview__modal').hide();
+    $("body").css("overflow", "unset");
   });
 
   $(window).on('click', function (e) {
     if ($(e.target).is('.c-intaview__modal')) {
-      modalIntaview.hide();
+      $('.c-intaview__modal').hide();
+      $("body").css("overflow", "unset");
     }
   });
+
 });
+
+/*-------------------------------------------------------------------------*/
+$(function () {
+  $(".c-recruit__boxcontent:eq(0)").show();
+  $(".c-recruit__tab").click(function () {
+    $('.c-recruit__tab').removeClass('is-active');
+    $(this).addClass("is-active");
+    const n = $(".c-recruit__tab").index(this);
+    $(".c-recruit__boxcontent").hide();
+    $(".c-recruit__boxcontent:eq(" + n + ")").fadeIn(100);
+  })
+})
